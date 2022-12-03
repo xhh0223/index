1360d445f2aa86c4f107b769bfd978af249fff05
xhh0223 <xhh0409@outlook.com>
2022-12-03 23:40:56 +0800

    feat(可以建立路由，连接数据库)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 pub mod modules;
 use modules::routes::RouterServiceCenter;
+use sqlx::mysql::MySqlPoolOptions;
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://root:root@127.0.0.1:3306/xhh_github_index")
+        .await?;
+    let tt1_rows = sqlx::query("select * from test ")
+        .fetch_all(&pool)
+        .await;
+
+    for row in tt1_rows {
+        println!("{:?}",row);
+    }
+    
     if let Some(router_service_center) = RouterServiceCenter::get_single_instance() {
         let app = router_service_center.get_app();
         axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
             .serve(app.into_make_service())
             .await
             .unwrap();
-    }
+    };
+
+    return Ok(());
 }
